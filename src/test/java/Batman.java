6c281e3fedc6c50f9606d156a28cd6b255c1df96
orazovalibek <orazalike@gmail.com>
2020-11-01 11:22:52 -0500

    Joker reject

--- src/test/java/Batman.java
+++ src/test/java/Batman.java
@@ -3,5 +3,6 @@ public class Batman {
         System.out.println("I will catch you Joker");
         // and Joker answered : "in next life"
         System.out.println("I'm going to catch you right now");
+        // Joker: "ha-ha-ha-ha" , try try
     }
 }
